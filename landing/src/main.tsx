2131c35c61259ca5f18c5d59dd8d0c0f72041d8d
diwenne <diwenh5@icloud.com>
2025-11-24 19:48:26 -0800

    analytics

--- landing/src/main.tsx
+++ landing/src/main.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'; // Assuming you have a global CSS file
+import { Analytics } from '@vercel/analytics/react';
 
 // This line initializes the i18next instance
 import './i18n';
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Suspense fallback="Loading...">
       <App />
+      <Analytics />
     </Suspense>
   </React.StrictMode>,
 );
